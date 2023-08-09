1dc35f271b8a149269436509d24f906118ebb881
CW-B-W <cw.brad.wang@gmail.com>
2023-08-10 02:16:07 +0800

    DON'T use flush

--- examples/master.cpp
+++ examples/master.cpp
@@ -19,7 +19,8 @@ void loop()
         const uint8_t buf[3+frame_data_bytes] = {0x55, 0x02, 'h', 'e', 'l', 'l', 'o', 0xA7};
         // 3 bytes for SYNC, PID and CHECKSUM
         swLin.write((const uint8_t*)buf, sizeof(buf));
-        swLin.flush(); // to ensure every bytes are sent.
+        // DO NOT use swLin.flush()
+        // swLin.flush() is for flushing Rx buffer
         swLin.endFrame();
         delay(10);
     }
